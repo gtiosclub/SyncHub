1e6a9b0ab851ab84bdefe243be6a4342973f0781
Steven Liu <lsystevee@gmail.com>
2026-02-15 09:28:05 -0500

    implemented signup/signin logic

--- SyncHub/Features/1. Navigation/AuthenticationView.swift
+++ SyncHub/Features/1. Navigation/AuthenticationView.swift
@@ -11,25 +11,67 @@ struct AuthenticationView: View {
     @Environment(AuthProvider.self) private var authProvider
     @State private var password = ""
     @State private var email = ""
+    @State private var signup = true
+    @Namespace private var animation
 
     var body: some View {
         VStack(alignment: .leading) {
-            TextField("email", text: $email).textFieldStyle(.roundedBorder)
+            credentialsView
 
-            TextField("password", text: $password).textFieldStyle(.roundedBorder)
-
-            Text("*Make sure you note your credentials down somewhere")
-                .font(.footnote).foregroundStyle(.secondary)
-
-
-            Button("Sign up") {
-                authProvider.signUp(email: email, password: password)
+            if signup {
+                signupView
+            } else {
+                signinView
             }
-            .frame(maxWidth: .infinity)
-            .buttonStyle(.borderedProminent)
-            .padding()
         }
         .padding()
+    }
+
+    @ViewBuilder
+    private var credentialsView: some View {
+        TextField("email", text: $email).textFieldStyle(.roundedBorder)
+        TextField("password", text: $password).textFieldStyle(.roundedBorder)
+    }
+
+    @ViewBuilder
+    private var signupView: some View {
+        Text("*Make sure you note your credentials down somewhere")
+            .font(.footnote).foregroundStyle(.secondary)
+
+        Button("Sign up") {
+            authProvider.signUp(email: email, password: password)
+        }
+        .frame(maxWidth: .infinity)
+        .buttonStyle(.borderedProminent)
+        .padding()
+
+        HStack {
+            Text("Have an account? ")
+            Button("Sign in") {
+               signup = false
+            }
+        }
+        .frame(maxWidth: .infinity)
+    }
+
+    @ViewBuilder
+    private var signinView: some View {
+        Text("*Make sure you note your credentials down somewhere")
+            .font(.footnote).foregroundStyle(.secondary).opacity(0)
+        Button("Sign in") {
+            authProvider.signIn(email: email, password: password)
+        }
+        .frame(maxWidth: .infinity)
+        .buttonStyle(.borderedProminent)
+        .padding()
+
+        HStack {
+            Text("Don't have an account yet? ")
+            Button("Sign up") {
+               signup = true
+            }
+        }
+        .frame(maxWidth: .infinity)
     }
 }
 
